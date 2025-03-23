210356655c2a2090d95a0ed56ed204e57b69c5ed
Brian <brian.kiprono8@student.moringaschool.com>
2025-03-23 22:07:36 +0300

    object modification

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+const employee ={
+    name:'Morio',
+    streetAddress: "Jaba Baze"
+};
+function updateEmployeeWithKeyAndValue(employee,name, streetAddress){
+    return {...employee, [name]: streetAddress}
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key, value){
+    employee[key]=value;
+    return employee;
+};
+function deleteFromEmployeeByKey(employee,key){
+    const newEmployee={...employee};
+    delete newEmployee[key];
+    return newEmployee;
+};
+function destructivelyDeleteFromEmployeeByKey(employee,key,value){
+    delete employee[key]
+    return employee;
+
+};
